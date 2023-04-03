5d311b15f925198284ccaf7993e15fcd2c7713c3
owinoalfred <ogzialfred@gmail.com>
2023-04-03 14:48:58 +0300

    inputting all the finished ks

--- bot.py
+++ bot.py
@@ -12,5 +12,12 @@ async def send_message(message, user_message, is_private):
 
 
 def run_disord_bot():
-    TOKEN:
-    CLIENT:
+    TOKEN: ' MTA5MjM5ODI3OTM1MTg2OTUyMA.G7-aJP.k9nZB3t86ZYVZrKU2C_2EGnLv4nv1GMJahn2Pc'
+    client: discord.Client()
+
+    @client.event
+
+    async def on_ready():
+        print(f'{client.user} is now running')
+
+        client.run('https://discord.com/api/oauth2/authorize?client_id=1092398279351869520&permissions=534723950656&scope=bot')
