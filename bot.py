d08cb68a4c4293bf9fed7dc16ea2162c6212d100
owinoalfred <ogzialfred@gmail.com>
2023-04-03 14:41:12 +0300

    adiing the functions to handle the responses from different users

--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
 import discord
 import responses 
 import main
+
+
+async def send_message(message, user_message, is_private): 
+    try:
+        responses = responses.handle_responses(user_message)
+        await message.author.send(responses) if is_private else await message.channel.send(responses)
