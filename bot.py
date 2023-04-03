18b1ff6c4aebc301a332102cb05650beefaa8d2d
owinoalfred <ogzialfred@gmail.com>
2023-04-03 15:46:13 +0300

    inputting all the finished ks

--- bot.py
+++ bot.py
@@ -1,23 +1,48 @@
 import discord
-import responses 
-import main
+import responses
 
 
-async def send_message(message, user_message, is_private): 
+# Send messages
+async def send_message(message, user_message, is_private):
     try:
-        responses = responses.handle_responses(user_message)
-        await message.author.send(responses) if is_private else await message.channel.send(responses)
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
     except Exception as e:
         print(e)
 
 
-def run_disord_bot():
-    TOKEN: ' MTA5MjM5ODI3OTM1MTg2OTUyMA.G7-aJP.k9nZB3t86ZYVZrKU2C_2EGnLv4nv1GMJahn2Pc'
-    client: discord.Client()
+
+
+
+def run_discord_bot():
+    TOKEN = 'M'
+    client = discord.Client()
 
     @client.event
-
     async def on_ready():
-        print(f'{client.user} is now running')
+        print(f'{client.user} is now running!')
 
-        client.run('')
+    @client.event
+    async def on_message(message):
+        # Make sure bot doesn't get stuck in an infinite loop
+        if message.author == client.user:
+            return
+
+        # Get data about the user
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        # Debug printing
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        # If the user message contains a '?' in front of the text, it becomes a private message
+        if user_message[0] == '?':
+            user_message = user_message[1:]  # [1:] Removes the '?'
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    # Remember to run your bot with your personal TOKEN
+    client.run(TOKEN)
